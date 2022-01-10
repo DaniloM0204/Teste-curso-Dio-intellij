4325d2cce04ed186a56a9bb96d039a6c93830b4f
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-01-09 22:26:43 -0300

    Inserindo exercício mensagem

---
+++ src/Exercicios_DIO/Mensagem.java
@@ -0,0 +1,25 @@
+package Exercicios_DIO;
+
+public class Mensagem {
+
+
+    public static void obterMsg(int hora) {
+
+        switch (hora) {
+            case 5, 6, 7, 8, 9, 10, 11, 12 -> msgBomDia();
+            case 13, 14, 15, 16, 17 -> msgBoaTarde();
+            case 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4 -> msgBoaNoite();
+            default -> System.out.println("Hora invalida");
+        }
+    }
+
+    public static void msgBomDia(){
+        System.out.println("Bom dia");
+    }
+    public static void msgBoaTarde(){
+        System.out.println("Bom Tarde");
+    }
+    public static void msgBoaNoite(){
+        System.out.println("Bom Noite");
+    }
+}
