fd44a7069724b4e75f4f140ca8956c7ea93bbf9b
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-01-09 22:13:32 -0300

    Inserindo exercício calculadora

--- src/Exercicios_DIO/Calculadora.java
+++ src/Exercicios_DIO/Calculadora.java
@@ -2,8 +2,34 @@ package Exercicios_DIO;
 
 public class Calculadora {
 
-    public static void soma(double numero1, double numero2){
+    public static void soma(double N1, double N2){
 
-        double resultado = numero1 + numero2;
+        double resultado = N1 + N2;
+
+        System.out.println("A soma de " + N1 + " mais " + N2 + " é " + resultado);
+
+    }
+
+    public static void subtracao(double N1, double N2) {
+
+        double resultado = N1 - N2;
+
+        System.out.println("A subtração de " + N1 + " menos " + N2 + " é " + resultado);
+
+    }
+
+    public static void multiplicacao(double N1, double N2) {
+
+        double resultado = N1 * N2;
+
+        System.out.println("A multiplicacao de " + N1 + " vezes " + N2 + " é " + resultado );
+
+    }
+
+    public static void divisao(double N1, double N2){
+
+        double resultado = N1 / N2;
+
+        System.out.println("A divisão entre " + N1 + " divido por " + N2 + " é " + resultado);
     }
 }
