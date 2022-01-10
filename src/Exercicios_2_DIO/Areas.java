dc0372e9de59e1263cd0f835da1f97b3ceacbe69
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-01-10 11:46:20 -0300

    Ajustando Main e Areas

--- src/Exercicios_2_DIO/Areas.java
+++ src/Exercicios_2_DIO/Areas.java
@@ -2,15 +2,15 @@ package Exercicios_2_DIO;
 
 public class Areas {
 
-    public static void area(double lado) {
+    public static void Quadrado(double lado) {
         System.out.println("Área do quadrado:" + lado*lado);
     }
 
-    public static void area(double lado1, double lado2){
+    public static void Retangulo(double lado1, double lado2){
         System.out.println("Área do retângulo:" + lado1 * lado2);
     }
 
-    public static void area(double Bmaior, double Bmenor, double H){
+    public static void Trapezio(double Bmaior, double Bmenor, double H){
         System.out.println("Área do trápezio:" + ((Bmaior+Bmenor)*H) / 2);
     }
 }
--- src/Exercicios_2_DIO/Main.java
+++ src/Exercicios_2_DIO/Main.java
@@ -1,4 +1,13 @@
 package Exercicios_2_DIO;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //Áreas
+        System.out.println("Exercício Área");
+        Areas.Quadrado(3);
+        Areas.Retangulo(5,5);
+        Areas.Trapezio(7,8,9);
+    }
 }
