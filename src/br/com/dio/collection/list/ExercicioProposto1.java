7dc442487bb0ff116b8e85248d19d68b15ea5f39
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-01-14 16:12:55 -0300

    Resolução Exercício 1

--- src/br/com/dio/collection/list/ExercicioProposto1.java
+++ src/br/com/dio/collection/list/ExercicioProposto1.java
@@ -1,41 +1,62 @@
 package br.com.dio.collection.list;
 
+/* Exercício proposto 1, faça um programa que receba a temperatura de 6 meses, faça a média
+e além de mostrar os meses e as temperaturas identifique os meses que tiveram temperatura acima
+da média.
+ */
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class ExercicioProposto1 {
-
-    public static void main(String[] args){
-        /* Fazer um programa que recebe a temperatura média
-         dos 6 primeiros meses do ano.*/
-
-        List<Temperatura> Temperatura = new ArrayList<>();
-        add(new Temperatura("Janeiro", 31));
-        add(new Temperatura("Fevereiro", 27));
-        add(new Temperatura("Março", 40));
-        add(new Temperatura("Abril", 33));
-        add(new Temperatura("Maio", 29));
-        add(new Temperatura("Junho", 35));
-
-        System.out.println("--\t Ordem de Inserção\t---");
-        System.out.println(Temperatura);
-    }
-
-    private static void add(Temperatura mes) {
-    }
-
     public ExercicioProposto1() {
     }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Double> temperaturasSemestral = new ArrayList();
+        double soma = 0.0D;
+
+        for(int i = 1; i <= 6; ++i) {
+            System.out.print("Qual a temperatura do mês " + i + ": ");
+            double temp = scan.nextDouble();
+            temperaturasSemestral.add(temp);
+            soma += temp;
+        }
+
+        double mediaTemperaturasSemenstral = soma / (double)temperaturasSemestral.size();
+        System.out.println("Temperaturas Semestral: " + temperaturasSemestral);
+        System.out.println("Média temperaturas Semestral: " + mediaTemperaturasSemenstral + "oC");
+        System.out.println("----------");
+        Iterator var7 = temperaturasSemestral.iterator();
+
+        while(var7.hasNext()) {
+            Double temp = (Double)var7.next();
+            if (temp > mediaTemperaturasSemenstral) {
+                int index = temperaturasSemestral.indexOf(temp);
+                switch(index) {
+                    case 0:
+                        System.out.println(index + 1 + " - JANEIRO: " + temp + " oC");
+                        break;
+                    case 1:
+                        System.out.println(index + 1 + " - FEVEREIRO: " + temp + " oC");
+                        break;
+                    case 2:
+                        System.out.println(index + 1 + " - MARÇO: " + temp + " oC");
+                        break;
+                    case 3:
+                        System.out.println(index + 1 + " - ABRIL: " + temp + " oC");
+                        break;
+                    case 4:
+                        System.out.println(index + 1 + " - MAIO: " + temp + " oC");
+                        break;
+                    case 5:
+                        System.out.println(index + 1 + " - JUNHO: " + temp + " oC");
+                }
+            }
+        }
+
+    }
 }
-
-class Temperatura{
-    public String mes;
-    public Integer Temp;
-
-    public Temperatura(String mes, Integer Temp) {
-        this.mes = mes;
-        this.Temp = Temp ;
-
-    }}
-
-
